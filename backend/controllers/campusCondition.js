dee159977bdc35f28956e0cfd524c3dc0d4fd1e7
SEE HONG CHEN <hongchensee8@gmail.com>
2024-03-17 11:53:15 +0800

    update comment backend function

--- backend/controllers/campusCondition.js
+++ backend/controllers/campusCondition.js
@@ -381,7 +381,7 @@ export const deleteCondition = async (req, res) => {
 		}
 
 		res.status(200).json({ msg: "Success", deletedCondition });
-	} catch {
+	} catch (err) {
 		console.log(err);
 
 		res.status(500).json({ error: err.message });
